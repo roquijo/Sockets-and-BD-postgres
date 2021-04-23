a0e0d6ab4a0126ca3d3efe92aa27d634013fed03
Juan Pablo Menses Bocanegra <juanweue@gmail.com>
2021-04-23 09:50:56 -0500

    MN

--- src/mvc/graphics/PlayerInfoPanel.java
+++ src/mvc/graphics/PlayerInfoPanel.java
@@ -1,4 +1,38 @@
 package mvc.graphics;
 
-public class PlayerInfoPanel {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PlayerInfoPanel extends JPanel implements ActionListener {
+
+
+
+    private JButton btnAgregarJugador;
+    private JButton btnEliminarJugador;
+    private JButton btnModificarJugador;
+
+    private JLabel jlblNombre;
+    private JLabel jlblIdentificacion;
+    private JLabel jlblidentificacion;
+    private JLabel jlblEdad;
+    private JLabel jlblPosicion;
+
+    //private JComboBox jComboBoxPlayer;
+
+    public PlayerInfoPanel(){
+
+        setLayout(new GridLayout(3,3));
+
+        jlblNombre = new JLabel("Nombre");
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
