15eb62ce0a7d7ee975c7d3498652fc7d8380091a
jorge <roquijo@hotmail.com>
2021-04-23 10:39:02 -0500

    mj

--- src/mvc/graphics/FacultyInfoPanel.java
+++ src/mvc/graphics/FacultyInfoPanel.java
@@ -1,10 +1,65 @@
 package mvc.graphics;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FacultyInfoPanel extends JPanel implements ActionListener {
+
+    private static final String CAMBIAR_JUGADOR = "CambiarJugador";
+
+    private InterfaceTournament principal;
+
+    private JLabel jlblNombre;
+    private JLabel jlblIdentificacion;
+    private JLabel jlblEdad;
+    private JLabel jlblPosicion;
+
+    private JComboBox comboJugadores;
+    private JTextField txtIdentificacion;
+    private JTextField txtEdad;
+    private JTextField txtPosicion;
+
+
+
+
+
+    public FacultyInfoPanel(InterfaceTournament ventana){
+
+        principal = ventana;
+
+        setLayout(new GridLayout(3,3));
+
+
+        jlblNombre = new JLabel("Nombre");
+        jlblIdentificacion = new JLabel("Identificacion");
+        jlblEdad = new JLabel("Edad");
+        jlblPosicion = new JLabel("Posicion");
+
+        comboJugadores = new JComboBox( );
+        comboJugadores.setEditable( false );
+        comboJugadores.addActionListener( this );
+        comboJugadores.setActionCommand( CAMBIAR_JUGADOR );
+        comboJugadores.setPreferredSize( new Dimension( 230, 20 ) );
+
+        txtIdentificacion = new JTextField();
+        txtEdad = new JTextField();
+        txtPosicion = new JTextField();
+
+        add(jlblNombre);
+        add(comboJugadores);
+
+        add(jlblIdentificacion);
+        add(txtIdentificacion);
+
+        add(jlblEdad);
+        add(txtEdad);
+
+        add(jlblPosicion);
+        add(txtPosicion);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
